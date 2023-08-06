d87a4bcdf00da501c9380ed247e4dfe319b457bd
pozhara <poliazhara04@gmail.com>
2023-08-06 11:16:12 +0000

    Loader.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,14 @@
-import css from './App.module.css';
-import NavBarSimple from './components/NavBarSimple';
+import css from "./App.module.css";
+import Sidebar from "./components/Sidebar";
+import NavBarForm from "./components/NavBarForm";
+import Content from "./components/Content";
 
 function App() {
   return (
     <div className={css.App}>
-      <NavBarSimple/>
+      <NavBarForm />
+      <Sidebar />
+      <Content />
     </div>
   );
 }
