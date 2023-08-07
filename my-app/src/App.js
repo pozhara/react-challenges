dbf1813ec6c6614f323393f299ed6442cc46e0a2
pozhara <poliazhara04@gmail.com>
2023-08-07 13:08:50 +0000

    ContentAPIHooks

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,14 @@
 import css from "./App.module.css";
 import Sidebar from "./components/Sidebar";
 import NavBarForm from "./components/NavBarForm";
-import ContentHooks from "./components/ContentHooks";
+import ContentAPIHooks from "./components/ContentAPIHooks";
 
 function App() {
   return (
     <div className={css.App}>
       <NavBarForm />
       <Sidebar />
-      <ContentHooks />
+      <ContentAPIHooks />
     </div>
   );
 }
